2500ea1294bff44ebe8b494cbc5e56661d6844ba
Rhys Lees <43909932+RhysLees@users.noreply.github.com>
2022-10-14 17:24:58 +0100

    nova-kit/nova-packages-tool

--- nova.mix.js
+++ nova.mix.js
@@ -20,16 +20,9 @@ class NovaExtension {
 
   webpackConfig(webpackConfig) {
     webpackConfig.externals = {
-      vue: 'Vue',
-    }
-
-    webpackConfig.resolve.alias = {
-      ...(webpackConfig.resolve.alias || {}),
-      'laravel-nova': path.join(
-        __dirname,
-        'vendor/laravel/nova/resources/js/mixins/packages.js'
-      ),
-    }
+			vue: "Vue",
+			"laravel-nova": "LaravelNova",
+		};
 
     webpackConfig.output = {
       uniqueName: this.name,
